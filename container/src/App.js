9d3e65fb3440bf71a25e0ead3d60d59a1aab4bd1
Sachin Hegde <sachin.ananth.hegde@gmail.com>
2023-08-27 17:45:09 +0530

    Container public path config

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return <div>
-      <h1>Hi There</h1>
+      <h1>Welcome</h1>
       <MarketingApp />
     </div>
 };
